ea245ab9e5cb4b4d7df2f93e425985904b6893ac
David Bowman <dlbowman11@gmail.com>
2017-01-04 13:58:07 -0500

    added todos

--- server.js
+++ server.js
@@ -1,10 +1,59 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var todos = [{
+    id: 1,
+    description: 'Mow the yard',
+    completed : false
+}, {
+    id: 2,
+    description: 'Get groceries.',
+    completed : false
+}, {
+    id: 3,
+    description: 'Go to work.',
+    completed: true
+}];
 
 app.get('/', function (req, res){
     res.send('TODO API Root');
 });
+
+//GET /todos
+app.get('/todos', function(req, res){
+   res.json(todos); 
+});
+
+//GET /todos/:id
+app.get('/todos/:id', function (req, res){
+    var todoId = parseInt(req.params.id);
+    var matched;
+    
+    //res.send('Asking for todo with id of ' + req.params.id);
+
+    for(var i = 0; i< todos.length ; i++)
+        {
+            if(todoId === todos[i].id)
+                {
+                    matched = todos[i];
+                    //res.json(todos[i]);
+                }
+        }
+    if(matched)
+        {
+            res.json(matched);
+        }else{
+            res.status(404).send("Not Found");
+        }
+    
+
+
+
+    //Call if no match
+
+});
+
+
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + ".");
 });
